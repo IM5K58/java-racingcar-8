268f253e92d75dfe46e1c295445a0b74ac348558
IM5K58 <gitue11@gmail.com>
2025-10-23 20:45:06 +0900

    feat(Output): 출력 기능 일부 구현

---
+++ src/main/java/racingcar/view/Output.java
@@ -0,0 +1,13 @@
+package racingcar.view;
+
+public class Output {
+
+    public void What_is_name(){
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+    }
+
+    public void How_many_trials(){
+        System.out.println("시도할 횟수는 몇 회인가요?");
+    }
+
+}
