e658e06279e724710d0d17d925eaf2cbc652b3fd
IM5K58 <gitue11@gmail.com>
2025-10-23 20:33:30 +0900

    feat(Input): 입력 기능 추가

---
+++ src/main/java/racingcar/view/Input.java
@@ -0,0 +1,15 @@
+package racingcar.view;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class Input {
+
+    public String input_name(){
+        return Console.readLine();
+    }
+
+    public int input_trials(int n){
+        return n;
+    }
+
+}
