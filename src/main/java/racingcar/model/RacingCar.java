27136b86b77697657b7df284edf079b0624ffe0a
IM5K58 <gitue11@gmail.com>
2025-10-24 00:07:27 +0900

    feat(RacingCar): 자동차 정보 관리를 위한 RacingCar 클래스 생성 후 기능 구현

---
+++ src/main/java/racingcar/model/RacingCar.java
@@ -0,0 +1,31 @@
+package racingcar.model;
+
+import camp.nextstep.edu.missionutils.Randoms;
+public class RacingCar {
+    private final String carName;
+    private int raceProgressCount;
+
+    static final int RANGE_START = 0;
+    static final int RANGE_END = 9;
+    static final int RANGE_LIMIT = 4;
+    static final int MOVE_VALUE = 1;
+    static final String MOVE_SIGN = "-";
+
+    public RacingCar(String carName){
+        //if(!carName 검증) throw IllegalArgumentException("자동차 이름이 잘못 설정 되었습니다.");
+        this.carName = carName;
+        raceProgressCount = 0;
+    }
+
+    public void race(){
+        int randomNum = Randoms.pickNumberInRange(RANGE_START,RANGE_END);
+        if(randomNum >= RANGE_LIMIT){
+            raceProgressCount += MOVE_VALUE;
+        }
+    }
+
+    public void raceResult(){
+        System.out.print(carName + " : ");
+        System.out.println(MOVE_SIGN.repeat(raceProgressCount));
+    }
+}
