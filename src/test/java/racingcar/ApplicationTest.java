28a70be3edd4cd9a22adb4fbc3d26d830a3a22ab
gitue <gitue11@gmail.com>
2025-10-27 21:05:59 +0900

    test: 테스트 코드 추가

--- src/test/java/racingcar/ApplicationTest.java
+++ src/test/java/racingcar/ApplicationTest.java
@@ -1,7 +1,15 @@
 package racingcar;
 
+import camp.nextstep.edu.missionutils.test.Assertions;
 import camp.nextstep.edu.missionutils.test.NsTest;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
 
 import static camp.nextstep.edu.missionutils.test.Assertions.assertRandomNumberInRangeTest;
 import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
@@ -12,6 +20,7 @@ class ApplicationTest extends NsTest {
     private static final int MOVING_FORWARD = 4;
     private static final int STOP = 3;
 
+    @DisplayName("기본 상황에서의 출력")
     @Test
     void 기능_테스트() {
         assertRandomNumberInRangeTest(
@@ -23,6 +32,7 @@ class ApplicationTest extends NsTest {
         );
     }
 
+    @DisplayName("중복된이름이 입력에 있을 시 처리 확인")
     @Test
     void 기능_테스트_중복_이름_입력() {
         assertRandomNumberInRangeTest(
@@ -34,6 +44,24 @@ class ApplicationTest extends NsTest {
         );
     }
 
+    @DisplayName("여러 경우의 테스트")
+    @ParameterizedTest
+    @MethodSource("tester")
+    void 다중_테스트_실행(String input, String trial) {
+        assertSimpleTest(() -> {
+            run(input, trial);
+        });
+    }
+    static Stream<Arguments> tester() {
+        return Stream.of(
+                Arguments.of("pobi,kyum,won,jang","5"),
+                Arguments.of("pobi,kyum,won,jang","10"),
+                Arguments.of("won,won,won,won","6")
+
+        );
+    }
+
+    @DisplayName("5글자 이상의 문자열이 들어간 경우의 예외 처리 확인")
     @Test
     void 예외_테스트_5글자_이상() {
         assertSimpleTest(() ->
@@ -42,6 +70,8 @@ class ApplicationTest extends NsTest {
         );
     }
 
+
+    @DisplayName("이름으로 적은 글자에 숫자나 알파벳을 제외한 문자가 포함된 경우")
     @Test
     void 예외_테스트_예외_문자사용() {
         assertSimpleTest(() ->
